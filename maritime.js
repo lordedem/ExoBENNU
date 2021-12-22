b680d4fe3b0d1166218495c1acc6767745dfddc7
lordedem <65088542+lordedem@users.noreply.github.com>
2021-12-22 17:00:36 +0000

    Update maritime.js

--- maritime.js
+++ maritime.js
@@ -6,3 +6,36 @@ var maritime = ['Lome','Tsevie','Vogan','Aneho','Tabligbo']
         maritime = maritime.filter(item => item !== city1);
         console.log("PlayoffCity1: " +city1);
     }
+// var maritime = ['Lome','Tsevie','Vogan','Aneho','Tabligbo']
+// // console.log(maritime)
+// for (let i = 0; i < maritime.length; i++){ <<le probleme est du au fait qu'a la 3e execution la taille de i devient superieure a celle de l'array>>
+    
+// // for (let i = 0 , j=  maritime.length ;i<j; i++) {
+//     console.log([i, maritime.length]);
+//     // console.log(maritime);
+//         let city1 = maritime[Math.floor(Math.random() * maritime.length)]
+        
+//         maritime = maritime.filter(item => item !== city1);
+//         console.log("PlayoffCity1: " +city1);
+//         console.log(maritime);
+
+//     }
+    
+var  maritime = ['Lome','Tsevie','Vogan','Aneho','Tabligbo'];
+
+// for (let city of maritime) {
+//     console.log(maritime);
+//     city = maritime[Math.floor(Math.random() * maritime.length)]
+//     maritime = maritime.filter(item => item !== city);
+//     console.log(city);
+    
+// }
+
+for (let city in maritime) {
+    console.log(maritime);
+    city = maritime[Math.floor(Math.random() * maritime.length)]
+    maritime = maritime.filter(item => item !== city);
+    console.log(city);
+    
+}
+console.log(maritime);
